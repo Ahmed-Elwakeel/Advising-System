630a76d6030530972f45490a6ef94aebed8690b6
Ahmed ElWakeel <ahmed.o.elwakeel@gmail.com>
2016-12-09 14:41:42 +0200

    credit hours + pre

---
+++ Advising System.pl
@@ -0,0 +1,135 @@
+:- use_module(library(csv)).
+:- use_module(library(clpfd)).
+:- ['cur.pl'].
+:- ['obl.pl'].
+
+readFile(File , Rows , RowName):-
+                csv_read_file(File, Rows , [functor(RowName)]),
+                maplist(assert, Rows).
+
+
+
+test(X):-
+         X = [A,B,C],
+         X ins 0..2,
+         testHelper(X),
+         labeling([],X).
+         
+testHelpe([H|T]):-
+              H #\= 0 ,
+              testHelper(T).
+
+advising_system(Chs ,Ncourses,TLLCourses,Schedule):-
+        readFile('data_formated_22556.csv' , Schedule , schRow),
+        %readFile('history.csv' , History , histrow),
+        %getList(Cur),
+        getObl(OblCourses),
+Cur = [curRow('CSEN 503',4,['CSEN 501']),curRow('AE 101',2,[]),curRow('DE 101',2,[]),curRow('CHEMt 102',4,[]),curRow('As 102',4,['AE 101'])],
+History = [histrow('AE 101','F'),histrow('CHEMt 102','B')],
+%Cur = [curRow('CSEN 503',4,['CSEN 501'])],
+%History = [histrow('AE 101','F'),histrow('As 102','B'),histrow('CHEMt 102','B')],
+        hc(Cur, History, Ncourses1),       % remove courses with a grade not equal F from cur
+        checkPre(Ncourses1, History, Ncourses),
+        length(Schedule , ScheduleSize),
+        length(Ncourses , CoursesSize),
+        NewCoursesSize #= CoursesSize * 3,
+        length(TLLCourses, NewCoursesSize),  %TLLCourses represent the number of courses that could be taken multiplied by 3 to account for the lecture, lab, and tutorial.
+        TLLCourses ins 0..ScheduleSize,  %0 means there is not lecture , tutorial , or lab for this course. else, it represent a row in the schedule.
+
+        checkCorrectness(0 ,TLLCourses , Ncourses, Schedule ,OblCourses, Chs ), %After checkCorrectness ,   TLLCourses is correct, such that each 3 consecutive elements are matching correctly with the element in the Ncourses.
+
+
+        labeling([],TLLCourses).
+
+
+
+
+% remove courses with a grade not equal F from cur
+
+hc(Cur , [], Cur).
+hc(Cur , [histrow(Course , Grade)|T] , Out):-
+                           Grade ='F',
+                           hc(Cur , T ,Out).
+hc(Cur , [histrow(Course , Grade)|T], Out1):-
+                           Grade \= 'F',
+                           hc(Cur , T ,Out),
+                           delete(Out,curRow(Course,_,_),Out1).
+
+
+
+% create a list of all the courses that could be taken == > the result of hc/3
+cList([],[]).
+cList([curRow(Course,_,_)|T],[A|B]):- cList(T,B),
+                                      A = Course.
+
+
+
+% After checkCorrectness ,   TLLCourses is correct, such that each 3 consecutive elements are matching correctly with the element in the Ncourses.
+% check that the summation of all the credit hours equals the given credit hours
+checkCorrectness(ChsSoFar , []  ,  [],  Schedule ,OblCourses, Chs):- ChsSoFar #=Chs.
+
+checkCorrectness(ChsSoFar , [T,Lec,Lab|Tail]  ,  [curRow(Course,Ch,Pre) | TC],  Schedule ,OblCourses, Chs) :-
+                                             T #= 0,
+                                             Lec #= 0,
+                                             Lab #=0 ,
+                                             \+(member(Course , OblCourses)), %obligatory courses that the student won’t be able to skip
+                                             checkCorrectness(ChsSoFar, Tail , TC , Schedule ,OblCourses,Chs).
+checkCorrectness(ChsSoFar , [T,Lec,Lab|Tail]  ,  [curRow(Course,Ch,Pre) | TC],  Schedule ,OblCourses, Chs):-
+
+                                    checkT( [T,Lec,Lab |Tail]  ,  [curRow(Course,Ch,Pre) | TC],  Schedule),
+                                    checkLec([T,Lec,Lab |Tail]  ,  [curRow(Course,Ch,Pre) | TC],  Schedule),
+                                    checkLab([T,Lec,Lab |Tail]  ,  [curRow(Course,Ch,Pre) | TC],  Schedule),
+
+                                    X1 #= ChsSoFar + Ch ,
+
+                                    checkCorrectness(X1 , Tail , TC , Schedule,OblCourses, Chs).
+
+% check if it is 0 in one of the elements , then check that it doesnot exist in the schedule so it is true.
+% if it is not 0 then the number (T , Lec , Lab) should represent a row in the schedule with the same course in Ncourses and the same thing( lab , tut ,lecture).
+checkT([T,Lec,Lab|Tail]   , [curRow(Course,Ch,Pre) | TC],  Schedule):-  T #= 0 , \+(member( schRow(Course, _, _, _, 'Tut', _, _) , Schedule )).
+checkT([T,Lec,Lab|Tail]   , [curRow(Course,Ch,Pre) | TC],  Schedule):-  T #\= 0, nth1(T , Schedule, schRow(Course, _, _, _, 'Tut', _, _)).
+
+checkLec([T,Lec,Lab|Tail]   , [curRow(Course,Ch,Pre) | TC],  Schedule):- Lec #= 0, \+(member( schRow(Course, _, _, _, 'Lecture', _, _) , Schedule )).
+checkLec([T,Lec,Lab|Tail]   , [curRow(Course,Ch,Pre) | TC],  Schedule):- Lec #\= 0, nth1(Lec , Schedule, schRow(Course, _, _, _, 'Lecture', _, _)).
+                                   
+checkLab([T,Lec,Lab|Tail]  , [curRow(Course,Ch,Pre) | TC],  Schedule):- Lab #= 0, \+(member( schRow(Course, _, _, _, 'Lab', _, _) , Schedule )).
+checkLab([T,Lec,Lab|Tail]  , [curRow(Course,Ch,Pre) | TC],  Schedule):- Lab #\= 0, nth1(Lab , Schedule, schRow(Course, _, _, _, 'Lab', _, _)).
+
+
+
+% check that the summation of all the credit hours equals the given credit hours
+%checkCreditHours(Chs , [] , [] ,  Chs , History).
+%checkCreditHours(ChsSoFar , [0 , 0 , 0 |Tail] ,[curRow(Course,Ch,Pre) | TC] ,  Chs,History):- checkCreditHours(ChsSoFar , Tail, TC ,Chs, History).
+%checkCreditHours(ChsSoFar , [T , Lec , Lab |Tail] ,[curRow(Course,Ch,Pre) | TC] ,  Chs , History):-
+%                            (T #\= 0;
+%                            Lec #\= 0;
+%                            Lab #\= 0),
+%                            X1 #= ChsSoFar + Ch,
+%                            %checkPreHelper(Pre, History),
+%                            checkCreditHours(X1 , Tail , TC ,Chs, History).
+
+
+%A student cannot take a course unless they have attended all its prerequisites
+checkPre([],History,[]).
+checkPre( [curRow(Course,Ch,Pre) | TC ] ,History, X ):-
+                            \+(checkPreHelper(Pre, History)),
+                            checkPre(TC,History,X).
+checkPre( [curRow(Course,Ch,Pre) | TC ] , History , [curRow(Course,Ch,Pre) |T] ):-
+                            checkPreHelper(Pre, History),
+                            checkPre(TC,History,T).
+
+checkPreHelper([], History).
+checkPreHelper([H|T], History):-
+
+                       member(histrow(H,Grade) , History),
+                       Grade \= 'F',
+                       checkPreHelper(T , History).
+                       
+
+
+
+
+
+
+
+
---
+++ cur.pl
@@ -0,0 +1,19 @@
+getList(CurList):-
+				CurList = [
+				curRow('CSEN 503',4,['CSEN 501','CSEN 502']),
+				curRow('AE 101',2,[]),curRow('DE 101',2,[]),
+				curRow('PHYS 101',4,[]),curRow('CSEN 102',4,[]),
+				curRow('MATH 103',8,[]),curRow('CHEMt 102',4,[]), 
+				curRow('As 102',2,['AE 101']),curRow('CSEN 301',4,['CSEN 102']),
+				curRow('DE 202',2,['DE 101']),curRow('MATH 301',8,['MATH 103']),
+				curRow('PHYSt 301',4,['PHYS 101']),curRow('ELCT 201',4,[]),
+				curRow('CSEN 501',4,['CSEN 102','CSEN 301']),curRow('RPW 401',4,['AE 101','As 102','SM 10','CPS 402']),
+				curRow('ELCT 301',4,['PHYSt 301','ELCT 201','CSEN 301']),curRow('SM 10',4,['AE 101','As 102']),
+				curRow('MATH 501',4,['MATH 301','MATH 103']),curRow('PHYSp 301',4,['PHYS 101','PHYSt 301']),
+				curRow('CPS 402',4,['AE 101','As 102','SM 10']),curRow('DE 303',4,['DE 101','DE 202']),
+				curRow('DE 404',4,['DE 303','DE 101','DE 202']),curRow('CSEN 503',4,['CSEN 501']),
+				curRow('CSEN 502',4,['CSEN 501']),curRow('CSEN 701',4,['CSEN 503']),
+				curRow('CSEN 702',4,['CSEN 701']),curRow('CSEN 703',4,['CSEN 702']),
+				curRow('CSEN 704',4,['CSEN 703'])
+					].
+						
